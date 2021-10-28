3af4ff87d6b8d55237c0db01b092726e0846f732
shreevalikushe <shreevalikushe@gmail.com>
2021-10-28 22:21:52 +0530

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function Navbar(){
+function Navbar() {
     let div = document.createElement("div")
     div.id = "navbar"
     div.innerHTML = `<img class="logo" src="https://logos-download.com/wp-content/uploads/2016/06/BBC_logo_black_background.png">
@@ -17,39 +17,63 @@ function Navbar(){
     <img id="searchIcon" src="https://as1.ftcdn.net/v2/jpg/03/25/73/68/1000_F_325736897_lyouuiCkWI59SZAPGPLZ5OWQjw2Gw4qY.jpg">`
     return div
 }
-export const fetchData =  async({category= "general",pageSize = 25},page= 1)=>{
+export const fetchData = async ({ category = "general", pageSize = 25 }, page = 1) => {
     return fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=65de7cf43ee34250a7fad72afaea85d5&category=${category}&page=${page}&pageSize=${pageSize}`)
-    .then((res)=>{
-        return res.json()
-    })
-    .then((res)=>{
-        return res.articles
-    })
+        .then((res) => {
+            return res.json()
+        })
+        .then((res) => {
+            return res.articles
+        })
 }
 
 
-export const showData = async(data,container)=>{
+export const showData = async (data, container) => {
     console.log(container)
     let cont = document.getElementById(container)
     cont.innerHTML = null;
     let div = document.createElement("div")
-    
-    for(let news of data){
-        let newsArticle = await handleNews(news)
-        div.append(newsArticle)
+    div.className = "innerDiv"
+    if (container === "container_1") {
+        for (let news of data) {
+            let newsArticle = await main(news)
+            div.append(newsArticle)
+        }
     }
+    else {
+        for (let news of data) {
+            let newsArticle = await handleNews(news)
+            div.append(newsArticle)
+        }
+    }
+
     cont.append(div)
+}
+const main = async (data) => {
+    let box = document.createElement("div")
+    box.className = "boxes";
+    box.style.background = `url(${data.urlToImage})`
+    let h4 = document.createElement("h4")
+    h4.textContent = `${data.title}`
+    h4.style.fontSize = "16px"
+    let publishDate = document.createElement("p")
+    publishDate.textContent = `Published At: ${data.publishedAt}`
+    box.append(h4, publishDate)
+    return box
 }
 const handleNews = async(data)=>{
     let box = document.createElement("div")
-    box.className = "boxes" ;
-    box.style.background = `url(${data.urlToImage})`
-    let p = document.createElement("p")
-    p.textContent = `${data.title}`
-    p.style.fontSize = "16px"
+    box.className = "section";
+    
+    let img = document.createElement("img")
+    img.src = `${data.urlToImage}`
+     
+    let h5 = document.createElement("h5")
+    h5.textContent = `${data.title}`
+    h5.style.fontSize = "16px"
     let publishDate = document.createElement("p")
-    publishDate.textContent = `${data.publishedAt}`
-    box.append(p,publishDate)
+    publishDate.textContent = `Published At: ${data.publishedAt}`
+    box.append(img,h5, publishDate)
     return box
 }
 export default Navbar
