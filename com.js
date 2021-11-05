99961c75ff26df2c16ed32994fc47758fb50650a
Utkarsh Kumar <kumarutkarsh2505@gmail.com>
2021-11-05 19:53:45 +0530

    remove

--- com.js
+++ com.js
@@ -1,24 +1,9 @@
-function Navbar() {
-    let div = document.createElement("div")
-    div.id = "navbar"
-    div.innerHTML = `<img class="logo" src="https://logos-download.com/wp-content/uploads/2016/06/BBC_logo_black_background.png">
-    <img class="signInLogo" src="https://as2.ftcdn.net/v2/jpg/00/65/77/27/500_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg">
-    <p class="signIn">Sign in</p>
-    <p>Home</p>
-    <p>News</p>
-    <p>Sport</p>
-    <p>Reel</p>
-    <p>Worklife</p>
-    <p>Travel</p>
-    <p>Future</p>
-    <p>Culture</p>
-    <p>More &#8964;  </p> 
-    <input id="search" type="search" placeholder="Search" name="search"> 
-    <img id="searchIcon" src="https://as1.ftcdn.net/v2/jpg/03/25/73/68/1000_F_325736897_lyouuiCkWI59SZAPGPLZ5OWQjw2Gw4qY.jpg">`
-    return div
-}
- let fetchData =  ({ category = "general", pageSize = 25 , page = 1 }) => {
-   return fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=e36b5c825d4e4a27b3b262445a398ffd&category=${category}&page=${page}&pageSize=${pageSize}`)
+
+
+/*e36b5c825d4e4a27b3b262445a398ffd */
+/* 198bd3c9c502495b83502cc5a2321c98*/ 
+ let fetchData = async ({ category = "general", pageSize = 25 , page = 1 }) => {
+   return fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=198bd3c9c502495b83502cc5a2321c98&category=${category}&page=${page}&pageSize=${pageSize}`)
         .then(function(res){
         return res.json()
     })
@@ -39,6 +24,12 @@ function Navbar() {
     if (container === "top") {
         for (let news of data) {
             let newsArticle = await handleTop(news)
+            div.append(newsArticle)
+        }
+    }
+    else if(container === "main_right"){
+        for (let news of data) {
+            let newsArticle = await handleside(news)
             div.append(newsArticle)
         }
     }
@@ -83,20 +74,44 @@ const handleTop = (data)=>{
     return box;
 
 }
+const handleside = (data)=>{
+    let box = document.createElement("div");
+    box.className = "top_div";
+    let img = document.createElement("img");
+    img.className = "img_box_top";
+    img.src = `${data.urlToImage}`;
+    let div1 = document.createElement("div");
+    let p = document.createElement("h3");
+    p.textContent = `${data.title}`;
+    let publishDate = document.createElement("p")
+    publishDate.style.padding = "1rem 0rem";
+    publishDate.textContent = `Published At: ${data.publishedAt}`
+    
+
+   
+    div1.append(p,publishDate)
+    box.append(img,div1);
+    return box;
+
+}
 const handleNews = (data) =>{
    // console.log(data);
     let box = document.createElement('div');
+    box.style.paddngTop = "30px"
     box.className = "boxes";
     let img = document.createElement("img");
     img.className = "img_box";
     img.src = `${data.urlToImage}`;
     let p = document.createElement("h3");
     p.textContent = `${data.title}`;
+    let publishDate = document.createElement("p")
+    publishDate.style.padding = "1rem 0rem";
+    publishDate.textContent = `Published At: ${data.publishedAt}`
     
 
-    box.append(img,p)
+    box.append(img,p,publishDate)
     return box;
 
 }
 
-export {Navbar,fetchData,loadData,handleNews}
+export {fetchData,loadData,handleNews}
