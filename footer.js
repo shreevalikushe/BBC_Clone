d6d35ee2118621b8bea14df40a92114e5cab36cb
a4aks <a4ashish04@gmail.com>
2021-11-05 10:42:06 +0530

    File added

---
+++ footer.js
@@ -0,0 +1,29 @@
+
+export const addFooter = async () =>{
+    const footer = `
+    <h3>Explore the BBC</h3>
+    <div id = "id1">
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="index.html">News</a></li>
+        <li><a href="index.html">Entertainment</a></li>
+        <li><a href="index.html">Health</a></li>
+        <li><a href="index.html">Science</a></li>
+        <li><a href="index.html">Sports</a></li>
+        <li><a href="index.html">Technology</a></li>
+    </ul>
+    <hr/>
+    <ul>
+        <li><a href="index.html">Terms of Use</a></li>
+        <li><a href="index.html">About the BBC</a></li>
+        <li><a href="index.html">Privacy Policy</a></li>
+        <li><a href="index.html">Cookies</a></li>
+        <li><a href="index.html">Accessibility Help</a></li>
+        <li><a href="index.html">Parental Guidence</a></li>
+        <li><a href="index.html">Contact the BBC</a></li>
+        <li><a href="index.html">Get Personalised NewsLetters</a></li>
+    </ul>
+    <p>Copyright © 2021 BBC. The BBC is not responsible for the content of external sites. Read about our approach to external linking.</p>
+</div>`;
+return footer;
+}
